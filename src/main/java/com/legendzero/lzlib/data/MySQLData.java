1735443f5e6e897d202a947a58a23b71758803cf
CrypticStorm <crypticstorm.gamersconference@gmail.com>
2015-04-01 19:46:48 -0400

    Add MySQL support.

---
+++ src/main/java/com/legendzero/lzlib/data/MySQLData.java
@@ -0,0 +1,23 @@
+package com.legendzero.lzlib.data;
+
+import com.legendzero.lzlib.database.MySQL;
+
+public class MySQLData implements SQLData<MySQL> {
+
+    private final MySQL mySQL;
+
+    public MySQLData(MySQL mySQL) {
+        this.mySQL = mySQL;
+    }
+
+    @Override
+    public MySQL getStorage() {
+        return this.mySQL;
+    }
+
+    @Override
+    public boolean save() {
+        this.mySQL.closeConnection();
+        return true;
+    }
+}
