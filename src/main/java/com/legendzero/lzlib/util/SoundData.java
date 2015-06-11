c9513ef96e0a6eed19c4e8c496410454905f1856
CrypticStorm <crypticstorm.gamersconference@gmail.com>
2015-06-11 17:26:15 -0400

    Add SoundData for wrapping sound, volume, and pitch together.

---
+++ src/main/java/com/legendzero/lzlib/util/SoundData.java
@@ -0,0 +1,53 @@
+package com.legendzero.lzlib.util;
+
+import org.bukkit.Sound;
+import org.bukkit.entity.Player;
+
+import java.util.Arrays;
+import java.util.Iterator;
+
+public class SoundData {
+
+    private final Sound soundEnum;
+    private final String soundString;
+    private final float volume;
+    private final float pitch;
+
+    public SoundData(Sound soundEnum, float volume, float pitch) {
+        this.soundEnum = soundEnum;
+        this.soundString = null;
+        this.volume = volume;
+        this.pitch = pitch;
+    }
+
+    public SoundData(String soundString, float volume, float pitch) {
+        this.soundEnum = null;
+        this.soundString = soundString;
+        this.volume = volume;
+        this.pitch = pitch;
+    }
+
+    public void play(Player... players) {
+        if (this.soundEnum != null) {
+            Arrays.stream(players).forEach(player -> player.playSound(player.getLocation(), this.soundEnum, this.volume, this.pitch));
+        } else {
+            Arrays.stream(players).forEach(player -> player.playSound(player.getLocation(), this.soundString, this.volume, this.pitch));
+        }
+    }
+
+    public void play(Iterable<Player> players) {
+        if (this.soundEnum != null) {
+            players.forEach(player -> player.playSound(player.getLocation(), this.soundEnum, this.volume, this.pitch));
+        } else {
+            players.forEach(player -> player.playSound(player.getLocation(), this.soundString, this.volume, this.pitch));
+        }
+    }
+
+    public void play(Iterator<Player> players) {
+        if (this.soundEnum != null) {
+            players.forEachRemaining(player -> player.playSound(player.getLocation(), this.soundEnum, this.volume, this.pitch));
+        } else {
+            players.forEachRemaining(player -> player.playSound(player.getLocation(), this.soundString, this.volume, this.pitch));
+        }
+    }
+}
